6e5f7530d091e3dca71b8138ce804ab1f43f8d7a
Logan <logan.rose0329@gmail.com>
2022-01-18 23:30:28 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
+import { MatCommonModule } from '@angular/material/core';
 import { AppComponent } from './app.component';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    MatDialogModule,
+    BrowserModule,
+    MatCommonModule,
+    MatChipsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
